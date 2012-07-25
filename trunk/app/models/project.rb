933c77a172f00a22d9844dc955591face33befee
lujon <jonsson.ludde@gmail.com>
2012-07-25 12:57:01 +0200

    Removed rails tutorial stuff and fixed problem with the projects model

--- trunk/app/models/project.rb
+++ trunk/app/models/project.rb
@@ -4,6 +4,6 @@ class Project < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email,	presence: true, format: { with: VALID_EMAIL_REGEX }
 
-	validates :year,	presence: true, uniqueness: { case_sensative: false }
+	validates :year,	presence: true, uniqueness: { case_sensative: false },
 						numericality: { only_integer: true }
 end
