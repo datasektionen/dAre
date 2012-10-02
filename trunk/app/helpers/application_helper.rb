1ba0183bddde2b3de8b4e42811d5ddfd8eb6dadf
lujon <jonsson.ludde@gmail.com>
2012-10-02 03:13:18 +0200

    Små designdetaljer

--- trunk/app/helpers/application_helper.rb
+++ trunk/app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
     # Returns the full title on a per-page basis.
     def full_title(page_title)
-        base_title = "dAre"
+        base_title = "dAre.se"
         
         if page_title.empty?
             base_title
