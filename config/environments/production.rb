8d4f479550ab1d7181e1d1c5184d2971b7689c80
Jesper Bränn <brann.jesper@gmail.com>
2014-11-05 00:40:54 +0100

    Create config file for email and add settings for sending emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,9 @@ DAre::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.sendmail_settings = {:arguments => '-i' }
+  # config.action_mailer.delivery_method = :sendmail
+  # config.action_mailer.sendmail_settings = {:arguments => '-i' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 end
---
+++ config/initializers/mail.rb
@@ -0,0 +1,11 @@
+EmailConfig = YAML.load_file(Rails.root.join('config/email_config.yml'))[Rails.env]
+
+ActionMailer::Base.smtp_settings = {
+ :address               => EmailConfig['address'],
+ :domain                => EmailConfig['domain'],
+ :port                  => 25,
+ :authentication        => 'plain',
+ :enable_starttls_auto  => false,
+ :user_name             => EmailConfig['user_name'],
+ :password              => EmailConfig['password']
+}
